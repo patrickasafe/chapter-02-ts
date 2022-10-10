43dc1589ecaea93327b157f4a64f6d3635ccdc32
patrickasafe <patrickasafe@gmail.com>
2022-10-10 13:32:15 -0300

    App functions

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,60 @@
+import { useForm } from "react-hook-form"
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles"
+import * as zod from 'zod'
+
+
+interface NewCycleFormProps {
+  activeCycle: 
+}
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(1).max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export const NewCycleForm = ({activeCycle}: NewCycleFormProps) => {
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+
+  return (
+    <FormContainer>
+      <label htmlFor="">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Projeto 4" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        id="minutesAmount"
+        type="number"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
